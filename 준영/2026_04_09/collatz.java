700ef5dbebd031dd697d18a8fc0e2ad965b0349a
jjuny0326 <140596682+jjuny0326@users.noreply.github.com>
2026-04-09 18:51:46 +0900

    Add Collatz solution in collatz.java
    Implement Collatz conjecture solution in Java.

--- 준영/2026_04_09/collatz.java
+++ 준영/2026_04_09/collatz.java
@@ -1,1 +1,26 @@
-print.out.println("HI");
+import java.util.*;
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> coll = new ArrayList<>();
+        
+        while(true){
+            if (n == 1){
+                coll.add(n);
+                break;
+            }
+            else if (n % 2 == 0){
+                coll.add(n);
+                n /= 2;
+            }
+            else{
+                coll.add(n);
+                n = n * 3 + 1;
+            }
+        }
+        int[] answer = new int[coll.size()];
+        for (int i = 0; i < coll.size(); i++){
+            answer[i] = coll.get(i);
+        }
+        return answer;
+    }
+}
