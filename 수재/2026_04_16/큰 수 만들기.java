d8c298ba6ce5856f8bb45fa1a71d423363438dba
KOSOOJAE <166842025+chip446@users.noreply.github.com>
2026-04-16 10:14:39 +0900

    큰 수 만들기2

--- 수재/2026_04_16/큰 수 만들기.java
+++ 수재/2026_04_16/큰 수 만들기.java
@@ -1,25 +1,63 @@
-package ct;
-
-public class Main {
-    public static void main(String[] args) {
-        String number = "4177252841", answer = "";
-        int k = 4;
+// 시간 초과 --------------------------------------------------
+class Solution {
+    public String solution(String number, int k) {
+        String answer = "";
+        // 앞에서 부터 숫자를 읽을 때 순서를 저장
         int n = 0;
 
+        // String을 char형으로 하나씩 가져오기
+        // 전체 길이 - 제거 수 = 출력할 자리 수
         for (int i = 0; i < number.length()-k; i++) {
+            // 최댓값 저장
             int max = 0;
+            // 첫번째 자리가 이동가능 총 거리
+            // 0 ~ k + indexNum + 1 -> 뺀 숫자만큼 이동 가능하니까
             for (int j = n; j < k + i + 1; j++){
+                // 큰 수 저장
                 if (max < number.charAt(j) - '0') {
                     max = number.charAt(j) - '0';
+                    // 큰 수의 위치 저장
                     n = j;
                 }
-                System.out.println(max + ", " + n);
             }
             answer += max;
+            // 사용한 큰 수 다음 수부터 시작
             n += 1;
-            System.out.println("-----------------------------------");
         }
 
-        System.out.println(answer);
+        return answer;
+    }
+}
+
+// 시간 초과 해 --------------------------------------------------
+class Solution {
+    public String solution(String number, int k) {
+        String answer = "";
+        // 앞에서 부터 숫자를 읽을 때 순서를 저장
+        int n = 0;
+
+        // String을 char형으로 하나씩 가져오기
+        // 전체 길이 - 제거 수 = 출력할 자리 수
+        for (int i = 0; i < number.length()-k; i++) {
+            // 최댓값 저장
+            int max = 0;
+            // 첫번째 자리가 이동가능 총 거리
+            // 0 ~ k + indexNum + 1 -> 뺀 숫자만큼 이동 가능하니까
+            for (int j = n; j < k + i + 1; j++){
+                // 큰 수 저장
+                if (max < number.charAt(j) - '0') {
+                    max = number.charAt(j) - '0';
+                    // 큰 수의 위치 저장
+                    n = j;
+                    // 시간 초과: 9일 때 즉시 중지
+                    if (max == 9) break;
+                }
+            }
+            answer += max;
+            // 사용한 큰 수 다음 수부터 시작
+            n += 1;
+        }
+
+        return answer;
     }
 }
