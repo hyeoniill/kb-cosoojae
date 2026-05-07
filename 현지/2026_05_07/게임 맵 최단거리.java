08da162c83a0555fe9c28af745b2628037343257
HyunJi <gee031022@gmail.com>
2026-05-07 18:22:02 +0900

    Refactor pathfinding logic in 게임 맵 최단거리.java

--- 현지/2026_05_07/게임 맵 최단거리.java
+++ 현지/2026_05_07/게임 맵 최단거리.java
@@ -1,5 +1,4 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[][] maps) {
         int n = maps.length;
@@ -15,31 +14,51 @@ class Solution {
         // 시작 지점
         queue.add(new int[]{0, 0, 1});
         maps[0][0] = 0;
-
+        
         while(!queue.isEmpty()) {
             int[] current = queue.poll();
             int x = current[0];
             int y = current[1];
             int distance = current[2];
-
+            
             // 도착 거리바놘
             if (x == n - 1 && y == m - 1) {
                 return distance;
             }
-
-            // 사방탐색
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                // 맵 안벗어나고 검은거아니고
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m && maps[nx][ny] == 1) {
-                    maps[nx][ny] = 0; //방문
-                    queue.add(new int[]{nx, ny, distance + 1});
-                }
+            
+            // 위로 가기
+            int nx1 = x + dx[0];
+            int ny1 = y + dy[0];
+            if (nx1 >= 0 && nx1 < n && ny1 >= 0 && ny1 < m && maps[nx1][ny1] == 1) {
+                maps[nx1][ny1] = 0;
+                queue.add(new int[]{nx1, ny1, distance + 1});
+            }
+            
+            // 아래로 가기
+            int nx2 = x + dx[1];
+            int ny2 = y + dy[1];
+            if (nx2 >= 0 && nx2 < n && ny2 >= 0 && ny2 < m && maps[nx2][ny2] == 1) {
+                maps[nx2][ny2] = 0;
+                queue.add(new int[]{nx2, ny2, distance + 1});
+            }
+            
+            // 왼쪽으로 가기
+            int nx3 = x + dx[2];
+            int ny3 = y + dy[2];
+            if (nx3 >= 0 && nx3 < n && ny3 >= 0 && ny3 < m && maps[nx3][ny3] == 1) {
+                maps[nx3][ny3] = 0;
+                queue.add(new int[]{nx3, ny3, distance + 1});
+            }
+            
+            // 오른쪽으로 가기
+            int nx4 = x + dx[3];
+            int ny4 = y + dy[3];
+            if (nx4 >= 0 && nx4 < n && ny4 >= 0 && ny4 < m && maps[nx4][ny4] == 1) {
+                maps[nx4][ny4] = 0;
+                queue.add(new int[]{nx4, ny4, distance + 1});
             }
         }
-
+        
         //갈수없음
         return -1;
     }
